4d54960ac002b9709c094b4ac2ab7ac6d928ecd0
Henry409 <henry40982@gmail.com>
2022-02-02 07:32:15 -0600

    Attempt1 to push to gitHub

---
+++ TestApplication.java
@@ -0,0 +1,6 @@
+public class TestApplication {
+    public static void main(String[] args){
+        System.out.println("This is an attempt to use IntelliJ with Github.");
+        System.out.println("Did it work?");
+    }
+}
